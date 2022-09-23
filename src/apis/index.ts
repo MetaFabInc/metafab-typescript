c37d454d7a528c7ba9f7468bc590419055804d7d
iamarkdev <ark@nftworlds.com>
2022-09-23 10:55:41 -0700

    Initial commit

---
+++ src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ContractsApi';
+export * from './CurrenciesApi';
+export * from './GamesApi';
+export * from './PlayersApi';
+export * from './TransactionsApi';
+export * from './WalletsApi';
