dd6347c5cf85d4e7bc10dc600ed09bd4713a60fb
iamarkdev <ark@nftworlds.com>
2023-02-19 17:39:36 -0500

    SDK Update

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,6 +2,7 @@
 /* eslint-disable */
 export * from './ContractsApi';
 export * from './CurrenciesApi';
+export * from './EcosystemsApi';
 export * from './GamesApi';
 export * from './ItemsApi';
 export * from './LootboxesApi';
